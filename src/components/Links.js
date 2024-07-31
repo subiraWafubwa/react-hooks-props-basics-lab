c23ec137e53f1d601479260b124b936d7003674a
Subira Wafubwa <subira.craig.2023@gmail.com>
2024-07-31 20:58:50 +0300

    Passed Tests

---
+++ src/components/Links.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Links({ links ,linkedin, github }) {
+  return (
+    <div>
+        <h3>Links</h3>
+        {linkedin && <a href={linkedin}>{linkedin}</a>}
+        {github && <a href={github}>{github}</a>}
+    </div>
+  );
+}
+
+export default Links;
